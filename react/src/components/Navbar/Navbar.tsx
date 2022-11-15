55e569051ac39025db2746e79f5dc775d707185c
mchibane <mchibane@student.42.fr>
2022-11-15 18:45:11 +0100

    changed general styling and added styles/ directory

--- react/src/components/Navbar/Navbar.tsx
+++ react/src/components/Navbar/Navbar.tsx
@@ -17,7 +17,7 @@ export default function Navbar() {
 					<div className="menu_logo_wrap">
 						<div className="menu_logo">
 							<Link to={'/home'}>
-								<p style={{ margin: "0", fontSize: "25px" }}>MA TEUB</p>
+								<p style={{ margin: "0", fontSize: "25px" }}>PONG !</p>
 							</Link>
 						</div>
 					</div>
@@ -26,22 +26,34 @@ export default function Navbar() {
 							<nav>
 								<ul>
 									<li key="navbar-play">
-										<NavLink to={"/game"}>
+										<NavLink
+											to={"/game"}
+											style={({isActive}) => isActive ? {textDecoration: "underline"} : undefined}
+										>
 												PLAY
 										</NavLink>
 									</li>
 									<li key="navbar-chat">
-										<NavLink to={'/chat'}>
+										<NavLink
+											to={'/chat'}
+											style={({isActive}) => isActive ? {textDecoration: "underline"} : undefined}
+										>
 											CHAT
 										</NavLink>
 									</li>
 									<li key="navbar-leaderboard">
-										<NavLink to="/leaderboard">
+										<NavLink
+											to="/leaderboard"
+											style={({isActive}) => isActive ? {textDecoration: "underline"} : undefined}
+										>
 											LEADERBOARD
 										</NavLink>
 									</li>
 									<li key="navbar-profile">
-										<NavLink to="/profile">
+										<NavLink
+											to="/profile"
+											style={({isActive}) => isActive ? {textDecoration: "underline"} : undefined}
+										>
 											PROFILE
 										</NavLink>
 									</li>
