37c186f36b51cdf523f4866de013fd583f683de5
mchibane <mchibane@student.42.fr>
2022-11-15 18:45:40 +0100

    1st iteration of profile page implemented

--- react/src/routes/profile.tsx
+++ react/src/routes/profile.tsx
@@ -1,39 +1,74 @@
 import { useContext } from "react";
-import { useLoaderData } from "react-router-dom";
+import { redirect, useLoaderData } from "react-router-dom";
 import { AuthContext } from "../auth/AuthContext";
 import { getCookie } from "../requests/cookies"
 import { Navigate } from "react-router-dom";
 
-export function loader()
+import './styles/profile.css'
+
+export async function loader()
 {
-	// return getCookie('jwt');
-	// const auth = useContext(AuthContext);
-	// console.log(auth.isAuth);
-	return (fetch(`http://localhost:3333/users/me`, 
+	const user = await fetch(`http://localhost:3333/users/me/complete`,
 	{
 		method: 'GET',
-		headers: {"Authorization": "Bearer " + getCookie("jwt")}
-	}));
+		headers: {"Authorization": "Bearer " + getCookie('jwt')}
+	});
+	if (!user.ok)
+		redirect('/login');
+	// const ret = await user.json();
+	return (user);
 }
 
 
 export function Profile()
 {
 	const user: any = useLoaderData();
-	// user.catch( () => {return (<Navigate to={"/login"} />)});
-	// console.log(user);
-	if (user.statusCode) {
-		// console.log("user ds le cul");
-		return (
-			<Navigate to={"/login"} />
-		);
-	}
-	const auth = useContext(AuthContext);
+	const playerStats = user.playStats;
 
 	return (
-		<div>
-			<h1>Profile</h1>
-			<p>{user.userName}</p>
+		<div className="profilePage">
+			{/* <h1>Profile</h1> */}
+			<div className="profileTitle">
+				<div className="profileImg">
+					<img src={user.avatar} />
+				</div>
+				<div className="profileInfo">
+					<h3>{user.userName}</h3>
+					<p>{user.email}</p>
+				</div>
+			</div>
+			<div className="profilePong">
+				<div className="profileStatsContainer">
+					<h2>Stats</h2>
+					<div className="profileStats">
+						{
+							playerStats ?
+							<>
+								<p>Wins : {playerStats.wins}</p>
+								<p>Losses : {playerStats.losses}</p>
+								<p>Rank : {playerStats.rank}</p>
+								<p>Points Scored : {playerStats.points}</p>
+								<p>Achievement points : {playerStats.achievement_point}</p>
+							</>
+							:
+							<p>Something went wrong ! <br/> Check with the owner of this awesome webapp</p>
+						}
+					</div>
+				</div>
+				<div className="profileHistoryContainer">
+					<h2>Match History</h2>
+					{
+						user.matchHistory > 0 ?
+						(
+							<ul>
+								<li>There is a match history</li>
+							</ul>
+						)
+						:
+						<h3>No match played yet !</h3>
+					}
+				</div>
+			</div>
 		</div>
 	)
 }
