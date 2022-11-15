4f01b2b3f9316216e9a74be55472d3b7fb6563e7
mchibane <mchibane@student.42.fr>
2022-11-15 18:41:45 +0100

    added new route /me/complete to get complete user infos

--- nest/src/users/users.controller.ts
+++ nest/src/users/users.controller.ts
@@ -1,60 +1,76 @@
-import { Body, Controller, Get, Param, Patch, Post, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, NotFoundException, Param, Patch, Post, Query, UseGuards } from '@nestjs/common';
 import { Prisma, User } from '@prisma/client';
+import { NotFoundError } from 'rxjs';
 import { JwtGuard } from 'src/auth42/guard/jwt.guard';
 import { GetUser } from './decorator';
-import {CreateUserDto, UpdateUserDto} from './dto/users.dto'
+import { CreateUserDto, UpdateUserDto } from './dto/users.dto'
 import { UsersService } from './users.service';
 
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UsersController
 {
-    constructor(private readonly userService: UsersService) {}
+	readonly userIncludeAll: Prisma.UserInclude;
 
-    @Get('/me')
-    async getMe(@GetUser() user: User)
-    {
-        // console.log(user);
-        return (user);
-    }
+	constructor(private readonly userService: UsersService) {
+		this.userIncludeAll =
+		{
+			friends: true,
+			friendTo: true,
+			blacklist: true,
+			blacklistedBy: true,
+			matchHistory: true,
+			playStats: true,
+			channels: true,
+			messages: true
+		}
+	}
 
-    @Get()
-    async findAll()
-    {
-        const res = await this.userService.users({});
-        //if (!res)
-            // error handling
-        return res;
-    }
+	@Get('/me')
+	async getMe(@GetUser() user: User) {
+		// console.log(user);
+		return (user);
+	}
 
-    @Get(':userName')
-    async findOne(@Param('userName') userName: string)
-    {
-        const res = await this.userService.user( {userName});
-        // error handling
-        return res;
-    }
+	@Get('/me/complete')
+	async getFullProfile(@GetUser('id') id: string) {
+		const ret = await this.userService.getUserModal({ id }, this.userIncludeAll);
+		if (!ret) {
+			throw (new NotFoundException(`Cannot find user with id: ${id}`));
+		}
+		return (ret);
+	}
 
-    @Post()
-    async create(@Body() dto: CreateUserDto)
-    {
-        const data : Prisma.UserCreateInput = {...dto};
-        data.playStats = { 
-            create: {}
-        }
-        const res = await this.userService.createUser(data);
-        return res;
-    }
+	@Get()
+	async findAll() {
+		const res = await this.userService.users({});
+		//if (!res)
+		// error handling
+		return res;
+	}
 
-    @Patch(':userName')
-    async update(@Body() dto: UpdateUserDto, @Param('userName') userName: string)
-    {
-        const data: Prisma.UserUpdateInput = {...dto};
-        const res = await this.userService.updateUser({
-            where: {userName},
-            data
-        });
-        // error handling
-        return res;
-    }
+	@Get(':userName')
+	async findOne(@Param('userName') userName: string) {
+		const res = await this.userService.user({ userName });
+		// error handling
+		return res;
+	}
+
+	@Post()
+	async create(@Body() dto: CreateUserDto) {
+		const res = await this.userService.createUser(dto);
+		console.log({ res });
+		return res;
+	}
+
+	@Patch(':userName')
+	async update(@Body() dto: UpdateUserDto, @Param('userName') userName: string) {
+		const data: Prisma.UserUpdateInput = { ...dto };
+		const res = await this.userService.updateUser({
+			where: { userName },
+			data
+		});
+		// error handling
+		return res;
+	}
 }
--- nest/src/users/users.service.ts
+++ nest/src/users/users.service.ts
@@ -1,61 +1,77 @@
 import { Injectable } from '@nestjs/common';
-import { Prisma, User } from '@prisma/client';
+import { PlayStats, Prisma, User } from '@prisma/client';
+import { create } from 'domain';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 type userFindManyParams =
-{
-    skip?: number;
-    take?: number;
-    cursor?: Prisma.UserWhereUniqueInput;
-    where?: Prisma.UserWhereInput;
-    orderBy?: Prisma.UserOrderByWithRelationInput;
-}
+	{
+		skip?: number;
+		take?: number;
+		cursor?: Prisma.UserWhereUniqueInput;
+		where?: Prisma.UserWhereInput;
+		orderBy?: Prisma.UserOrderByWithRelationInput;
+	}
 
 @Injectable()
-export class UsersService
-{
-    constructor(private readonly prisma: PrismaService) {}
-    
-    async user(userWhereUniqueInput: Prisma.UserWhereUniqueInput) : Promise<User | null>
-    {
-        return this.prisma.user.findUnique(
-            {
-                where: userWhereUniqueInput
-            });
-    }
-        
-    async users(params: userFindManyParams) : Promise<User[]>
-    {
-        const {skip, take, cursor, where, orderBy} = params;
-        return this.prisma.user.findMany(
-            {
-                skip,
-                take,
-                cursor,
-                where,
-                orderBy
-            });
-    }
+export class UsersService {
 
-    async createUser(
-        data: Prisma.UserCreateInput) : Promise<User>
-    {
-        return this.prisma.user.create(
-            {
-                data
-            });
-    }
+	constructor(private readonly prisma: PrismaService) {
 
-    async updateUser( params: {
-        where: Prisma.UserWhereUniqueInput;
-        data: Prisma.UserUpdateInput;
-    }) : Promise<User>
-    {
-        const {data, where} = params;
-        return this.prisma.user.update(
-            {
-                data,
-                where
-            });
-    }
+	}
+
+	async user(userWhereUniqueInput: Prisma.UserWhereUniqueInput): Promise<User | null> {
+		return this.prisma.user.findUnique(
+			{
+				where: userWhereUniqueInput
+			});
+	}
+
+	async users(params: userFindManyParams): Promise<User[]> {
+		const { skip, take, cursor, where, orderBy } = params;
+		return this.prisma.user.findMany(
+			{
+				skip,
+				take,
+				cursor,
+				where,
+				orderBy
+			});
+	}
+
+	async createUser(
+		data: Prisma.UserCreateInput): Promise<User> {
+		return this.prisma.user.create(
+			{
+				data:
+				{
+					...data,
+					playStats:
+					{
+						create: {}
+					}
+				}
+			});
+	}
+
+	async updateUser(params: {
+		where: Prisma.UserWhereUniqueInput;
+		data: Prisma.UserUpdateInput;
+	}): Promise<User> {
+		const { data, where } = params;
+		return this.prisma.user.update(
+			{
+				data,
+				where
+			});
+	}
+
+	async getUserModal(where: Prisma.UserWhereUniqueInput, include: Prisma.UserInclude): Promise<User> {
+
+		const ret = await this.prisma.user.findUnique({
+			where,
+			include
+		});
+
+		return (ret);
+	}
 }
