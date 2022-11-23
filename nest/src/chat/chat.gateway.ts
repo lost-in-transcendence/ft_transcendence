041d07f065cf91ff0180b6bc77dd6e92d0253011
mchibane <mchibane@student.42.fr>
2022-11-23 16:12:35 +0100

    created socket adapter created server socket.io adapter which adds a middleware to server which checks credentials validity before connection and appends the current user object to the Socket.data object

--- nest/src/chat/chat.gateway.ts
+++ nest/src/chat/chat.gateway.ts
@@ -1,5 +1,16 @@
-import { WebSocketGateway, SubscribeMessage, MessageBody, WebSocketServer, ConnectedSocket, OnGatewayConnection, WsException } from '@nestjs/websockets';
-import { Server, Socket } from 'socket.io';
+import { Logger } from '@nestjs/common';
+import {
+	WebSocketGateway,
+	SubscribeMessage,
+	MessageBody,
+	WebSocketServer,
+	ConnectedSocket,
+	OnGatewayConnection,
+	WsException,
+	OnGatewayDisconnect,
+	OnGatewayInit
+} from '@nestjs/websockets';
+import { Server, Socket, Namespace } from 'socket.io';
 import { User } from '@prisma/client';
 import { JwtService } from '@nestjs/jwt';
 import { env } from 'process';
@@ -11,8 +22,11 @@ import { CreateChatDto } from './dto';
 import { GetUserWs } from 'src/users/decorator/get-user-ws';
 import { PrismaService } from 'src/prisma/prisma.service';
 
-@WebSocketGateway({cors: true})
-export class ChatGateway implements OnGatewayConnection {
+@WebSocketGateway({ cors: true, namespace: 'chat' })
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit
+{
+	private readonly logger = new Logger(ChatGateway.name);
+
 	constructor(
 		private readonly chatService: ChatService,
 		private readonly channelService: ChannelsService,
@@ -20,26 +34,30 @@ export class ChatGateway implements OnGatewayConnection {
 		private readonly prisma: PrismaService) { }
 
 	@WebSocketServer()
-	server: Server;
+	server: Namespace;
 
-
+	afterInit()
+	{
+		this.logger.log('Chat Gateway initialized');
+	}
 
 	async handleConnection(client: Socket)
 	{
-		try
-		{
-			const token = client.handshake.headers.authorization.split(' ')[1];
-			const ret = this.jwt.verify(token, {secret: env.JWT_SECRET});
-			const user: User = await this.prisma.user.findUnique({where: {id: ret.id}});
-			// console.log({ret});
-			client.data.user = user;
-			// console.log(client.data.user);
-		}
-		catch (err)
-		{
-			console.error('oki', {err});
-			client.disconnect();
-		}
+		this.logger.log('in connection');
+		// try
+		// {
+		// 	const token = client.handshake.headers.authorization.split(' ')[1];
+		// 	const ret = this.jwt.verify(token, { secret: env.JWT_SECRET });
+		// 	const user: User = await this.prisma.user.findUnique({ where: { id: ret.id } });
+		// 	// console.log({ret});
+		// 	client.data.user = user;
+		// 	// console.log(client.data.user);
+		// }
+		// catch (err)
+		// {
+		// 	this.logger.error('oki', {err});
+		// 	client.disconnect();
+		// }
 	}
 
 	handleDisconnect(client: Socket)
@@ -51,7 +69,7 @@ export class ChatGateway implements OnGatewayConnection {
 	@SubscribeMessage('message')
 	sendMessage(@MessageBody('text') text: string)
 	{
-		console.log("args", {text});
+		console.log("args", { text });
 		console.log(`sending text: ${text}`);
 		this.server.emit('message', text);
 	}
@@ -59,7 +77,7 @@ export class ChatGateway implements OnGatewayConnection {
 	@SubscribeMessage('joinRoom')
 	join(@MessageBody('channel') channel: string, @ConnectedSocket() client: Socket, @GetUserWs() user: User)
 	{
-		console.log(`client: ${client.id} join room ${channel}`, {user});
+		console.log(`client: ${client.id} join room ${channel}`, { user });
 		client.join(channel);
 		throw new WsException('test exception');
 		console.log(`client rooms :`, client.rooms.keys());
@@ -68,7 +86,7 @@ export class ChatGateway implements OnGatewayConnection {
 	@SubscribeMessage('toRoom')
 	toRoom(@MessageBody() body: CreateChatDto, @ConnectedSocket() client: Socket)
 	{
-		console.log('in toRoom', {body});
+		console.log('in toRoom', { body });
 		client.to(body.channel).emit('message', body);
 	}
 
---
+++ nest/src/websocket-server/socket-io.adapter.ts
@@ -0,0 +1,55 @@
+import { INestApplicationContext, Logger } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { IoAdapter } from "@nestjs/platform-socket.io";
+import { User } from "@prisma/client";
+import { Server, Socket } from "socket.io";
+import { env } from "process";
+
+import { PrismaService } from "src/prisma/prisma.service";
+
+// type SocketWithAuth = Socket['data'] & User;
+
+
+export class SocketIOAdapter extends IoAdapter
+{
+	private readonly logger = new Logger(SocketIOAdapter.name);
+
+	constructor(private readonly app: INestApplicationContext)
+	{
+		super(app);
+	}
+
+	createIOServer(port: number, options?: any)
+	{
+		this.logger.log('Creating Socket.io server');
+
+		const jwt = this.app.get(JwtService);
+		const prisma = this.app.get(PrismaService);
+		const server: Server = super.createIOServer(port);
+
+		server.of('chat').use(wsAuthMiddleWare(jwt, prisma, this.logger));
+
+		return (server);
+	}
+}
+
+const wsAuthMiddleWare = (jwt: JwtService, prisma: PrismaService, logger: Logger) =>
+	(socket: Socket, next) =>
+	{
+
+		logger.debug('In ws MiddleWare');
+
+		try
+		{
+			const token = socket.handshake.headers.authorization.split(' ')[1];
+			const decoded = jwt.verify(token, {secret: env.JWT_SECRET});
+			const user = prisma.user.findUnique({where: { id: decoded.id }});
+			socket.data.user = user;
+			next();
+		}
+		catch (err)
+		{
+			logger.error('ERROR')
+			next(new Error('Forbidden'));
+		}
+	}
