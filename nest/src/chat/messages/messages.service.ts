b01288853544a3a8b3bf8586fd7f1f3f8734fbe5
mchibane <mchibane@student.42.fr>
2022-11-25 15:14:03 +0100

    [wip] continuing work on chat

--- nest/src/chat/messages/messages.service.ts
+++ nest/src/chat/messages/messages.service.ts
@@ -1,14 +1,38 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, ImATeapotException, Injectable, Logger, NotFoundException, PreconditionFailedException } from "@nestjs/common";
+import { WsException } from "@nestjs/websockets";
+import { Message } from "@prisma/client";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 import { PrismaService } from "src/prisma/prisma.service";
 import { CreateMessageDto } from "./dto";
 
 @Injectable()
 export class MessagesService
 {
+	private readonly logger = new Logger(MessagesService.name);
+
 	constructor(private readonly prisma: PrismaService) {}
 
-	create(dto: CreateMessageDto)
+	async create(dto: CreateMessageDto): Promise<Message>
 	{
-
+		try
+		{
+			const newMessage = await this.prisma.message.create({
+				data: {
+					channel: {connect: {id: dto.channelId}},
+					sender: {connect: {id: dto.userId}},
+					content: dto.content
+				},
+			});
+			return (newMessage);
+		}
+		catch (err)
+		{
+			if (err instanceof PrismaClientKnownRequestError)
+			{
+				if (err.code === 'P2025')
+					throw new PreconditionFailedException('Record not found');
+			}
+			throw new ImATeapotException('Something unexpected happened');
+		}
 	}
 }
