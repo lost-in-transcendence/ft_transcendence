aca89e178b4bf9844ee34c809d9373162563f027
mchibane <mchibane@student.42.fr>
2022-12-06 18:58:43 +0100

    [merge] merge follow up

--- nest/src/websocket-server/interceptor/user.interceptor.ts
+++ nest/src/websocket-server/interceptor/user.interceptor.ts
@@ -4,25 +4,6 @@ import { Observable } from "rxjs";
 import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
-<<<<<<< HEAD
-export class UserInterceptor implements NestInterceptor {
-	constructor(private readonly prisma: PrismaService) {}
-
-	async intercept(context: ExecutionContext, next: CallHandler): Promise<Observable<any>> {
-		const	wsClient = context.switchToWs().getClient();
-		const	wsUser: User = wsClient.data.user;
-		const	dbUser = await this.prisma.user.findUnique({where: {id: wsUser.id}})
-		if (JSON.stringify(wsUser) !== JSON.stringify(dbUser))
-		{
-			console.log('dbUser !== wsUser');
-			console.log({dbUser}, {wsUser});
-			wsClient.data.user = dbUser;
-		}
-		console.log('COUCOU DE L\'INTERCEPTOR');
-		return next.handle();
-	}
-}
-=======
 export class UserInterceptor implements NestInterceptor
 {
 	constructor(private readonly prisma: PrismaService) { }
@@ -64,4 +45,3 @@ export class UserInterceptor implements NestInterceptor
 		return next.handle();
 	}
 }
->>>>>>> mchibane-chat
