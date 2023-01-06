1f38b1ae7bb4dad072c6dde4e8ab615bbbad467b
mchibane <mchibane@student.42.fr>
2023-01-06 19:24:47 +0100

    [dev] fixed merge confiilcts 3

--- react/src/components/Chat/rightBar/ContextMenu.tsx
+++ react/src/components/Chat/rightBar/ContextMenu.tsx
@@ -1,44 +1,40 @@
 import { useNavigate } from "react-router-dom";
 
 export function ContextMenu({
-  x,
-  y,
-  userName,
+	x,
+	y,
+	userName,
 }: {
-  x: number;
-  y: number;
-  userName: string;
-}) {
-  const navigate = useNavigate();
+	x: number;
+	y: number;
+	userName: string;
+})
+{
+	const navigate = useNavigate();
 
-  console.log("Rendering contextMenu");
-  console.log("x : " + x);
-  console.log("y : " + y);
+	function goToProfile(userName: string)
+	{
+		navigate(`/profile/view/${userName}`);
+	}
 
-  function goToProfile(userName: string) {
-    navigate(`/profile/view/${userName}`);
-  }
+	const liClassName: string =
+		"hover:bg-indigo-600 rounded cursor-pointer text-white";
 
-  const liClassName: string =
-    "hover:bg-indigo-600 rounded cursor-pointer text-white";
-
-  return (
-    <ul
-      className={`list-none w-48 rounded p-2 bg-zinc-800 fixed`}
-      style={{ top: `${y}px`, left: `${x}px` }}
-    >
-      <li
-        className={liClassName}
-        onClick={() => {
-          goToProfile(userName);
-        }}
-      >
-        Profile
-      </li>
-    </ul>
-  );
-}
-/*			/<li className={liClassName}>
+	return (
+		<ul
+			className={`list-none w-48 rounded p-2 bg-zinc-800 fixed`}
+			style={{ top: `${y}px`, left: `${x}px` }}
+		>
+			<li
+				className={liClassName}
+				onClick={() =>
+				{
+					goToProfile(userName);
+				}}
+			>
+				Profile
+			</li>
+			<li className={liClassName}>
 				Invite to play
 			</li>
 			<li className={liClassName}>
@@ -49,4 +45,8 @@ export function ContextMenu({
 			</li>
 			<li className={liClassName}>
 				Mute
-			</li>*/
+			</li>
+		</ul>
+	);
+}
+
