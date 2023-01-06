d6c0019393ab5824e9156808860734cbb0bb6b93
lebourre <55803027+LeoBourret@users.noreply.github.com>
2023-01-06 02:14:09 +0000

    [dev] just added more possible actions that came to my mind

--- react/src/components/Chat/rightBar/ContextMenu.tsx
+++ react/src/components/Chat/rightBar/ContextMenu.tsx
@@ -13,13 +13,19 @@ export function ContextMenu({ x, y, userName }: { x: number, y: number, userName
 	console.log(y)
 
 	return (
-		<div className={`w-32 rounded p-2 bg-zinc-800 absolute left-[${x}px] top-[${y}px]`}>
+		<div className={`w-48 rounded p-2 bg-zinc-800 absolute left-[${x}px] top-[${y}px]`}>
 			<li className={liClassName}
 				onClick={() => { goToProfile(userName) }}>
 				Profile
 			</li>
 			<li className={liClassName}>
-				Invite
+				Invite to play
+			</li>
+			<li className={liClassName}>
+				[conditional friend]
+			</li>
+			<li className={liClassName}>
+				Invite to channel >
 			</li>
 			<li className={liClassName}>
 				Mute
