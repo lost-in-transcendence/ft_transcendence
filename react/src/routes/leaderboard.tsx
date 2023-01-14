b28fa5e5e582c7b02a7a20d7bdd9a4a642b49774
NeronTheTyrant <61967568+NeronTheTyrant@users.noreply.github.com>
2023-01-14 22:45:35 +0000

    [dev] continued styling leaderboard

--- react/src/routes/leaderboard.tsx
+++ react/src/routes/leaderboard.tsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from "react";
-import { redirect, useLoaderData } from "react-router-dom";
+import { redirect, useLoaderData, useNavigate } from "react-router-dom";
 import SocketContext from "../components/Socket/socket-context";
 import { PlayStats } from "../dto/game.dto";
 import { backURL, getUserMe } from "../requests";
@@ -39,28 +39,180 @@ export function LeaderBoard()
 			<h1>
 				LEADERBOARD
 			</h1>
-			<div className="text-center text-white">
-				<h2>Hall of Fame</h2>
-				<div className="w-[480px] mx-auto flex bg-amber-200">
-					<div className="bg-red-600 flex-1">Hello</div>
-					<div className="bg-green-600 flex-1">Bye</div>
-					<div className="bg-blue-600 flex-1">Hawaii</div>
-				</div>
-			</div>
+			<HallOfFame ranking={ranking} />
 			<div>
 				{
 					ranking ?
 					<ul>
 						{ranking.map((v: PlayStats) =>
 						{
+							let userName = ''
+							if (v.user.userName)
+								userName = v.user.userName.length > 15 ? v.user.userName.slice(0, 15) + '...' : v.user.userName;
 							return (
 							<li key={v.userId} className="text-white border-4 border-green-400 flex gap-4 items-center">
-								<div>#{v.rank}</div>
+								<div className="w-[30px]">#{v.rank}</div>
 								<img className="w-[50px] rounded-full" src={`${backURL}/users/avatars/${v.user.userName}`} />
-								<div>{v.user.userName}</div>
+								<div className="w-[150px]">{userName}</div>
+								<div>{v.points}</div>
+								<div>{v.wins}</div>
+								<div>{v.losses}</div>
 							</li>
 							)
 						})}
+						<li key={'asdsdsa'} className="text-white border-4 border-green-400 flex gap-4 items-center">
+								<div>#0</div>
+								<img className="w-[50px] rounded-full" src={`https://www.pngkit.com/png/full/72-729913_user-blank-avatar-png.png`} />
+								<div>La Tua Madre</div>
+							</li>
+							<li key={'asdsdsa'} className="text-white border-4 border-green-400 flex gap-4 items-center">
+								<div>#0</div>
+								<img className="w-[50px] rounded-full" src={`https://www.pngkit.com/png/full/72-729913_user-blank-avatar-png.png`} />
+								<div>La Tua Madre</div>
+							</li>
+							<li key={'asdsdsa'} className="text-white border-4 border-green-400 flex gap-4 items-center">
+								<div>#0</div>
+								<img className="w-[50px] rounded-full" src={`https://www.pngkit.com/png/full/72-729913_user-blank-avatar-png.png`} />
+								<div>La Tua Madre</div>
+							</li>
+							<li key={'asdsdsa'} className="text-white border-4 border-green-400 flex gap-4 items-center">
+								<div>#0</div>
+								<img className="w-[50px] rounded-full" src={`https://www.pngkit.com/png/full/72-729913_user-blank-avatar-png.png`} />
+								<div>La Tua Madre</div>
+							</li>
+							<li key={'asdsdsa'} className="text-white border-4 border-green-400 flex gap-4 items-center">
+								<div>#0</div>
+								<img className="w-[50px] rounded-full" src={`https://www.pngkit.com/png/full/72-729913_user-blank-avatar-png.png`} />
+								<div>La Tua Madre</div>
+							</li>
+							<li key={'asdsdsa'} className="text-white border-4 border-green-400 flex gap-4 items-center">
+								<div>#0</div>
+								<img className="w-[50px] rounded-full" src={`https://www.pngkit.com/png/full/72-729913_user-blank-avatar-png.png`} />
+								<div>La Tua Madre</div>
+							</li>
+							<li key={'asdsdsa'} className="text-white border-4 border-green-400 flex gap-4 items-center">
+								<div>#0</div>
+								<img className="w-[50px] rounded-full" src={`https://www.pngkit.com/png/full/72-729913_user-blank-avatar-png.png`} />
+								<div>La Tua Madre</div>
+							</li>
+							<li key={'asdsdsa'} className="text-white border-4 border-green-400 flex gap-4 items-center">
+								<div>#0</div>
+								<img className="w-[50px] rounded-full" src={`https://www.pngkit.com/png/full/72-729913_user-blank-avatar-png.png`} />
+								<div>La Tua Madre</div>
+							</li>
+							
+							<li key={'asdsdsa'} className="text-white border-4 border-green-400 flex gap-4 items-center">
+								<div>#0</div>
+								<img className="w-[50px] rounded-full" src={`https://www.pngkit.com/png/full/72-729913_user-blank-avatar-png.png`} />
+								<div>La Tua Madre</div>
+							</li>
+							
+							<li key={'asdsdsa'} className="text-white border-4 border-green-400 flex gap-4 items-center">
+								<div>#0</div>
+								<img className="w-[50px] rounded-full" src={`https://www.pngkit.com/png/full/72-729913_user-blank-avatar-png.png`} />
+								<div>La Tua Madre</div>
+							</li>
+							<li key={'asdsdsa'} className="text-white border-4 border-green-400 flex gap-4 items-center">
+								<div>#0</div>
+								<img className="w-[50px] rounded-full" src={`https://www.pngkit.com/png/full/72-729913_user-blank-avatar-png.png`} />
+								<div>La Tua Madre</div>
+							</li>
+							<li key={'asdsdsa'} className="text-white border-4 border-green-400 flex gap-4 items-center">
+								<div>#0</div>
+								<img className="w-[50px] rounded-full" src={`https://www.pngkit.com/png/full/72-729913_user-blank-avatar-png.png`} />
+								<div>La Tua Madre</div>
+							</li>
+							<li key={'asdsdsa'} className="text-white border-4 border-green-400 flex gap-4 items-center">
+								<div>#0</div>
+								<img className="w-[50px] rounded-full" src={`https://www.pngkit.com/png/full/72-729913_user-blank-avatar-png.png`} />
+								<div>La Tua Madre</div>
+							</li>
+							<li key={'asdsdsa'} className="text-white border-4 border-green-400 flex gap-4 items-center">
+								<div>#0</div>
+								<img className="w-[50px] rounded-full" src={`https://www.pngkit.com/png/full/72-729913_user-blank-avatar-png.png`} />
+								<div>La Tua Madre</div>
+							</li>
+							<li key={'asdsdsa'} className="text-white border-4 border-green-400 flex gap-4 items-center">
+								<div>#0</div>
+								<img className="w-[50px] rounded-full" src={`https://www.pngkit.com/png/full/72-729913_user-blank-avatar-png.png`} />
+								<div>La Tua Madre</div>
+							</li>
+							<li key={'asdsdsa'} className="text-white border-4 border-green-400 flex gap-4 items-center">
+								<div>#0</div>
+								<img className="w-[50px] rounded-full" src={`https://www.pngkit.com/png/full/72-729913_user-blank-avatar-png.png`} />
+								<div>La Tua Madre</div>
+							</li>
+							<li key={'asdsdsa'} className="text-white border-4 border-green-400 flex gap-4 items-center">
+								<div>#0</div>
+								<img className="w-[50px] rounded-full" src={`https://www.pngkit.com/png/full/72-729913_user-blank-avatar-png.png`} />
+								<div>La Tua Madre</div>
+							</li>
+							<li key={'asdsdsa'} className="text-white border-4 border-green-400 flex gap-4 items-center">
+								<div>#0</div>
+								<img className="w-[50px] rounded-full" src={`https://www.pngkit.com/png/full/72-729913_user-blank-avatar-png.png`} />
+								<div>La Tua Madre</div>
+							</li>
+							<li key={'asdsdsa'} className="text-white border-4 border-green-400 flex gap-4 items-center">
+								<div>#0</div>
+								<img className="w-[50px] rounded-full" src={`https://www.pngkit.com/png/full/72-729913_user-blank-avatar-png.png`} />
+								<div>La Tua Madre</div>
+							</li>
+							<li key={'asdsdsa'} className="text-white border-4 border-green-400 flex gap-4 items-center">
+								<div>#0</div>
+								<img className="w-[50px] rounded-full" src={`https://www.pngkit.com/png/full/72-729913_user-blank-avatar-png.png`} />
+								<div>La Tua Madre</div>
+							</li>
+							<li key={'asdsdsa'} className="text-white border-4 border-green-400 flex gap-4 items-center">
+								<div>#0</div>
+								<img className="w-[50px] rounded-full" src={`https://www.pngkit.com/png/full/72-729913_user-blank-avatar-png.png`} />
+								<div>La Tua Madre</div>
+							</li>
+							<li key={'asdsdsa'} className="text-white border-4 border-green-400 flex gap-4 items-center">
+								<div>#0</div>
+								<img className="w-[50px] rounded-full" src={`https://www.pngkit.com/png/full/72-729913_user-blank-avatar-png.png`} />
+								<div>La Tua Madre</div>
+							</li>
+							<li key={'asdsdsa'} className="text-white border-4 border-green-400 flex gap-4 items-center">
+								<div>#0</div>
+								<img className="w-[50px] rounded-full" src={`https://www.pngkit.com/png/full/72-729913_user-blank-avatar-png.png`} />
+								<div>La Tua Madre</div>
+							</li>
+							<li key={'asdsdsa'} className="text-white border-4 border-green-400 flex gap-4 items-center">
+								<div>#0</div>
+								<img className="w-[50px] rounded-full" src={`https://www.pngkit.com/png/full/72-729913_user-blank-avatar-png.png`} />
+								<div>La Tua Madre</div>
+							</li>
+							<li key={'asdsdsa'} className="text-white border-4 border-green-400 flex gap-4 items-center">
+								<div>#0</div>
+								<img className="w-[50px] rounded-full" src={`https://www.pngkit.com/png/full/72-729913_user-blank-avatar-png.png`} />
+								<div>La Tua Madre</div>
+							</li>
+							<li key={'asdsdsa'} className="text-white border-4 border-green-400 flex gap-4 items-center">
+								<div>#0</div>
+								<img className="w-[50px] rounded-full" src={`https://www.pngkit.com/png/full/72-729913_user-blank-avatar-png.png`} />
+								<div>La Tua Madre</div>
+							</li>
+							<li key={'asdsdsa'} className="text-white border-4 border-green-400 flex gap-4 items-center">
+								<div>#0</div>
+								<img className="w-[50px] rounded-full" src={`https://www.pngkit.com/png/full/72-729913_user-blank-avatar-png.png`} />
+								<div>La Tua Madre</div>
+							</li>
+							<li key={'asdsdsa'} className="text-white border-4 border-green-400 flex gap-4 items-center">
+								<div>#0</div>
+								<img className="w-[50px] rounded-full" src={`https://www.pngkit.com/png/full/72-729913_user-blank-avatar-png.png`} />
+								<div>La Tua Madre</div>
+							</li>
+							<li key={'asdsdsa'} className="text-white border-4 border-green-400 flex gap-4 items-center">
+								<div>#0</div>
+								<img className="w-[50px] rounded-full" src={`https://www.pngkit.com/png/full/72-729913_user-blank-avatar-png.png`} />
+								<div>La Tua Madre</div>
+							</li>
+							<li key={'asdsdsa'} className="text-white border-4 border-green-400 flex gap-4 items-center">
+								<div>#0</div>
+								<img className="w-[50px] rounded-full" src={`https://www.pngkit.com/png/full/72-729913_user-blank-avatar-png.png`} />
+								<div>La Tua Madre</div>
+							</li>
+
 					</ul>
 					:
 					null
@@ -68,5 +220,84 @@ export function LeaderBoard()
 			</div>
 
 		</div>
+	)
+}
+
+
+export function HallOfFame({ranking} : {ranking: PlayStats[] | undefined})
+{
+	if (!ranking)
+		return null;
+
+	let number1: PlayStats | undefined = undefined;
+	let number2: PlayStats | undefined = undefined;
+	let number3: PlayStats | undefined = undefined;
+	if (ranking.length >= 1)
+		number1 = ranking.find((v) => {return v.rank === 1});
+	if (ranking.length >= 2)
+		number2 = ranking.find((v) => {return v.rank === 2});
+	if (ranking.length >= 3)
+		number3 = ranking.find((v) => {return v.rank === 3});
+
+
+
+	return (
+		<div className="text-center text-white">
+			<h2>Hall of Fame</h2>
+			<div className="w-[480px] mx-auto flex bg-white">
+				<Podium number={2} player={number2} />
+				<Podium number={1} player={number1} />
+				<Podium number={3} player={number3} />
+		</div>
+	</div>
+	)
+}
+
+export function Podium({number, player} : {number: number, player: PlayStats | undefined})
+{
+	const navigate = useNavigate();
+	let imageMargin: number = 0;
+	let divBg: string = ''
+	if (number === 1)
+	{
+		imageMargin = 5;
+		divBg = 'bg-green-600';
+	}
+	else if (number === 2)
+	{
+		imageMargin = 40;
+		divBg = 'bg-indigo-500';
+	}
+	else if (number === 3)
+	{
+		imageMargin = 60;
+		divBg = 'bg-slate-500';
+	}
+	let imgSrc: string = '';
+	if (!player)
+		imgSrc = 'https://www.pngkit.com/png/full/72-729913_user-blank-avatar-png.png'
+	else
+		imgSrc = `${backURL}/users/avatars/${player.user.userName}`
+	let userName: string = ''
+
+	if (!player || !player.user.userName)
+		userName = "No one :'(";
+	else
+	{
+		userName = player.user.userName.length > 12 ? player.user.userName.slice(0, 12) + "..." : player.user.userName;
+	}
+	const onClick = player ? () => {navigate(`/profile/view/${player?.user.userName}`)} : () => {};
+
+	return (
+		<div className={`flex-1 flex flex-col text-center ${player ? 'cursor-pointer' : ''}`} 
+		onClick={onClick}>
+					<div className="mb-[5px]" style={{marginTop: imageMargin + 'px'}}>
+						<img className="w-[80px] rounded-full m-auto" src={imgSrc}/>
+					</div>
+					<div className={`${divBg} w-full h-full border-y-[1px] border-l-[1px] border-black text-xl`} >
+						<p>#{number}</p>
+						<p className="text-xl pb-[5px]">{userName}</p>
+					</div>
+				</div>
 	)
 }
