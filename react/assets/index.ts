460a631ed89088f20d87bb6d78f7e9aba988443e
mchibane <mchibane@student.42.fr>
2023-01-18 16:52:34 +0100

    [dev] added placeholder img to assets folder in react

--- react/assets/index.ts
+++ react/assets/index.ts
@@ -2,8 +2,10 @@ import onlineIcon from  './online.png'
 import offlineIcon from './offline.png'
 import awayIcon from './away.png'
 import busyIcon from './busy.png'
+import avatarPlaceholder from './avatar_placeholder.jpeg'
 
 export { onlineIcon }
 export { offlineIcon }
 export { awayIcon }
 export { busyIcon }
+export { avatarPlaceholder }
